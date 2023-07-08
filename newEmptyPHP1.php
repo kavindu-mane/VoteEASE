c280beaf7e2242af7f4ce5711512f3a10574f572
Lakruwan Jayathissa <lakruwan987@gmail.com>
2023-07-08 13:46:24 +0530

    edit
    edit

--- newEmptyPHP1.php
+++ newEmptyPHP1.php
@@ -1,103 +1,9 @@
-<!DOCTYPE html>
-<?php
-
-// Establish a connection to the MySQL server
-$servername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "expensemanager";
-
-$conn = new mysqli($servername, $dbUsername, $dbPassword, $dbName);
-
-// Check the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sql = "SELECT * FROM users WHERE id='3'";
-$result = $conn->query($sql);
-if ($result->num_rows > 0) {
-    while ($row = $result->fetch_assoc()) {
-        $id = $row["id"];
-        $name = $row["FullName"];
-        $pos = $row["Possion"];
-        $mail = $row["email"];
-        $pno = $row["PhoneNumOffice"];
-        $pnp = $row["PhoneNumPersonal"];
-    }
-}
-$conn->close();
-?>
-
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>Document</title>
-
-        <!--add external files-->
-        <link rel="Stylesheet" href="css/navbar.css">
-        <script src="navbar.js"></script>
-
-        <!-- Font Awesome -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
-        <!-- Google Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
-        <!-- MDB -->
-        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css" rel="stylesheet"/>
-
-        <!-- MDB -->
-        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js"></script>
-
-    </head>
-    <body> 
-
-        <!-- Navbar -->
-        <nav id="main-navbar" class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
-            <!-- Container wrapper -->
-            <div class="container-fluid" >
-
-                <!-- Logo area -->
-                <a class="navbar-brand" class="navbar-nav d-flex justify-content-end align-items-center" href="#">
-                    <p> logo </p>
-                </a>
-
-                <!-- Right links -->
-                <ul  class="navbar-nav ms-auto d-flex flex-row"> 
-
-                    <li>
-                        <a class="nav-link" role="button">
-                            <img src="a.jpg" class="rounded-circle" height="40" alt="ProPic"/>
-                        </a>
-                    </li>
-                    <li>&nbsp;</li>            
-                </ul>
-            </div>
-            <!-- Container wrapper -->
-        </nav>
-    
-    
-    
 <section style="background-color: #eee; padding-top: 50px">    
   <div class="container py-5">
       
     <!--nav bar-->
     <br>
-      <div class="row">
-      <div class="col">
-        <nav class="bg-light rounded-3 p-3 mb-4">
-          <ol class="breadcrumb mb-0">
-            <li><a href="index.php">Home </a></li>
-            <li>&nbsp;&nbsp; > &nbsp;&nbsp;</li>
-            <li><a href="Our_team.php"> Our Team</a></li>
-            <li>&nbsp;&nbsp; > &nbsp;&nbsp;</li>
-            <li class="active">User Profile - <?php echo $name ?></li>
-          </ol>
-        </nav>
-      </div>
-    </div>
-    
+ 
     
     <!--user Information-->
         <div class="row">
@@ -221,7 +127,3 @@ $conn->close();
   </div>
   </div>
 </section>
-
-    
-</body>
-</html>
