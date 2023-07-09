d452284af65fbce15ac689c6eca02c8aee3a51b2
ThushariSenevirathne <thusharisenevirathne48@gmail.com>
2023-07-09 17:55:26 +0530

    add 2 files
    Login & Register pages

---
+++ forMe.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        
+        <!-- Font Awesome -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" rel="stylesheet"/>
+        <!-- Google Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet"/>
+        <!-- MDB -->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.css" rel="stylesheet"/>
+
+        <!-- MDB -->
+        <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/mdb-ui-kit/6.3.0/mdb.min.js"></script>
+        
+        <title></title>
+    </head>
+    <body>
+        
+        <section style="background-color: #eee; padding-top: 50px">    
+  <div class="container py-5">
+      
+    <!--nav bar-->
+    <br>
+      <div class="row">
+          <div class="col-lg-1"></div>    
+    <div class="col-lg-10">
+        <div class="card mb-8">
+        
+            <div class="card-body text-center"><H6>Enter your Details</h6></div>
+        </div>
+      </div>
+          <div class="col-lg-1"></div>
+    </div>
+    
+    <!--change password-->
+    <div class="row">
+        <div class="col-lg-1"></div>    
+         <div class="col-lg-10">
+        
+            <div class="card mb-8">
+                 <div class="card-body">
+                     <div class="row">
+
+                         <div class="col-sm-4"><p class="mb-0">Enter password</p></div>
+                         <div class="col-sm-8"><input type="password" name="password" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your password" /><br></div>
+                     </div>
+
+                     <div class="row">
+
+                         <div class="col-sm-4"><p class="mb-0">Confirm password</p></div>
+                         <div class="col-sm-8"><input type="password" name="password" id="form3Example3" class="form-control form-control-lg" placeholder="Confirm your password" /><br></div>
+                     </div>
+
+                 </div>
+                 <div class="col-lg-1"></div>
+             </div>
+
+    <!--user Information-->
+        <div class="row">
+        <div class="col-lg-1"></div>    
+         <div class="col-lg-10">
+        
+        
+        <div class="card mb-8">
+          <div class="card-body">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">ID</p></div>
+              <div class="col-sm-8"><input type="text" name="id" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your User Name" /></div>
+            </div><hr style="opacity: .05">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">Full Name</p></div>
+              <div class="col-sm-8"><input type="text" name="Full Name" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your Full Name" /></div>
+            </div><hr style="opacity: .05">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">Possition</p></div>
+              <div class="col-sm-8"><input type="text" name="Possition" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your Possition" /></div>
+            </div><hr style="opacity: .05">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">Email</p></div>
+              <div class="col-sm-8"><input type="text" name="email" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your Email" /></div>
+            </div><hr style="opacity: .05">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">Phone Number(Office)</p></div>
+              <div class="col-sm-8"><input type="number" name="office NO" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your " /></div>
+            </div><hr style="opacity: .05">
+            <div class="row">
+              <div class="col-sm-4"><p class="mb-0">Phone Number(Personal)</p></div>
+              <div class="col-sm-8"><input type="number" name="Personal No" id="form3Example3" class="form-control form-control-lg" placeholder="Enter your mobile Number" /></div>
+          </div>
+            
+        </div>
+
+        
+    </div>
+            <div class="col-lg-1"></div>    
+
+        
+    
+  </div>
+  </div>
+</section>
+        
+     
+ 
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
