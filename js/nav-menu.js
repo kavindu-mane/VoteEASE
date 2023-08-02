2367666c7521ec20ca26ed99d6818f4db65a9b18
Kavindu Manahara <cst20003@std.uwu.ac.lk>
2023-08-02 14:31:42 +0530

    improve codes

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -1,14 +1,15 @@
 let menuOpen = false;
 const menuSettings = (e) => {
   const MENU = document.getElementById("menu");
+  const docElement = document.documentElement.classList;
   if (menuOpen) {
-    MENU.classList.remove("right-0");
-    MENU.classList.add("-right-80");
+    MENU.classList.replace("right-0", "-right-80");
+    docElement.remove("overflow-y-hidden");
     menuOpen = false;
     e.target.innerHTML = "menu";
   } else {
-    MENU.classList.remove("-right-80");
-    MENU.classList.add("right-0");
+    MENU.classList.replace("-right-80", "right-0");
+    docElement.add("overflow-y-hidden");
     menuOpen = true;
     e.target.innerHTML = "close";
   }
