62a3bc4162fa2b8ab509a014ddfe5b0d20f301f9
Chamara D Rodrigo <108047725+ChamaraRodrigo@users.noreply.github.com>
2023-08-11 14:41:14 +0530

    voter dashbord

--- src/main/java/com/voteease/classes/DBConnector.java
+++ src/main/java/com/voteease/classes/DBConnector.java
@@ -1,12 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package com.voteease.classes;
 
-import io.github.cdimascio.dotenv.Dotenv;
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class DBConnector {
-    public static Connection getConnection() throws Exception{
-        Dotenv dotenv = Dotenv.load();
-        Class.forName(dotenv.get("DRIVER"));
-        return DriverManager.getConnection(dotenv.get("URL"),dotenv.get("DB_USER"),dotenv.get("DB_PW"));
+
+        
+    
+   public class DBConnector {
+    
+    static final String DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/voteease";
+    static final String DB_USER = "root";
+    static final String DB_PASSWORD = "";
+    static Connection con;
+    
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        Class.forName(DRIVER);
+        con = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+        return con;
     }
 }
